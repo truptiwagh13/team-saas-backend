6472991223a96641b255f2563f0f9658d7b4a64e
Trupti wagh <trupti32813@gmail.com>
2025-11-22 11:31:34 +0530

    fixxing issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,24 @@
   import { UsersModule } from './users/users.module';
   import { AuthModule } from './auth/auth.module';
   import { TasksModule } from './tasks/tasks.module';
+  import {
+  ApolloServerPluginLandingPageLocalDefault,
+} from "@apollo/server/plugin/landingPage/default";
+
 
   @Module({
     imports: [
       ConfigModule.forRoot({ isGlobal: true }),
 
   GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: true,
-    sortSchema: true,
-    playground: false,
-    plugins: [],   // <-- IMPORTANT: disables express5 playground plugin
-  }),
+  driver: ApolloDriver,
+  autoSchemaFile: true,
+  sortSchema: true,
+  playground: false,
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault(),   // works with Apollo v5
+  ],
+}),
       SequelizeModule.forRootAsync({
         imports: [ConfigModule],
         inject: [ConfigService],
